b2f56710651e8942d42054132f92e493d4599784
esdxone <roaringgate@gmail.com>
2021-11-28 19:13:45 +0300

    tabs

--- js/script.js
+++ js/script.js
@@ -27,11 +27,25 @@ $(document).ready(function() {
         prevArrow: '<button type="button" class="slick-arrow--back" style=""><svg class="icon icon-left"> <use xlink:href="#icon-left"></use> </svg></button>',
         nextArrow: '<button type="button" class="slick-arrow--next" style=""><svg class="icon icon-right"> <use xlink:href="#icon-right"></use> </svg></button>',
       });
+
         //Продуктовый слайдер
         $('.pop-goods__product-list').slick({
             dots: false,
             arrows: false,
             slidesToShow: 4,
             slidesToScroll: 4,
+          });
+
+          //Сервис - табы
+          $('.js-tab-nav').click(function(e) {
+            e.preventDefault();
+            var id = $(this).attr('data-tab');//получаем дата-атрибут навигации
+            var tabContent = $('.js-tab-content[data-tab="'+ id +'"]'); //Получаем дата-атрибут таба
+
+            $('.js-tab-nav.active').removeClass('active');
+            $(this).addClass('active');
+
+            $('.js-tab-content.active').removeClass('active');
+            tabContent.addClass('active');
           });
     });
